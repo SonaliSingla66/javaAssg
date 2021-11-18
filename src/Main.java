270b62f3c7b157e15f2249158ed6c8b9afaf2d59
sonalisingla66 <sonalisingla66@gmail.com>
2021-11-19 01:03:01 +0530

    array assgn to count even nos

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+    int arr[]={3,2,8,1,4,7};
+    int n= arr.length;
+
+    CountingEven(arr,n);
+    }
+static void CountingEven(int arr[], int arr_size){
+        int even_count=0;
+        int odd_count=0;
+        for (int i=0; i< arr_size;i++){
+            if ((arr[i] & 1)== 1)
+                odd_count++;
+
+            else
+                even_count++;
+            }
+            System.out.println("number of even elements= "+even_count);
+    System.out.println("number of odd elements= "+odd_count);
+        }
+}
+
+
